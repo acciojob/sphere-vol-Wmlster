ecb0c4c7ed2315a3f87802728e94dd5efa52c167
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-23 20:19:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function volume_sphere() {
     //Write your code here
+
+	let r=document.getElementById("radius").value;
+	let v=document.getElementById("volume");
+
+	let num1=4/3;
+	
+	let num2=(num1)*3.14*r*r*r;
+
+	v.value=num2;
+	
   
 } 
 
